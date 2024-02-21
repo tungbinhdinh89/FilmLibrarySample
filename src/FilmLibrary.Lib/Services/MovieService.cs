49df2f3cc7c2be38a80c906589d718f1dee491fd
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-21 22:24:29 +0700

    Add dummy method add movie

--- src/FilmLibrary.Lib/Services/MovieService.cs
+++ src/FilmLibrary.Lib/Services/MovieService.cs
@@ -5,11 +5,19 @@ namespace FilmLibrary.Lib.Services
     public interface IMovieService
     {
         Task<MovieModel[]> SearchMoviesAsync(string? keyword = null);
+        Task AddNewMovieAsync(MovieModel movie);
+
+
     }
 
     public class MovieService : IMovieService
     {
         public Task<MovieModel[]> SearchMoviesAsync(string? keyword = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddNewMovieAsync(MovieModel movie)
         {
             throw new NotImplementedException();
         }

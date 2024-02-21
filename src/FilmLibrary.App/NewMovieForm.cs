781b9169b533a1ac74c390bbf3bcb40d400f0082
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-21 22:24:46 +0700

    connect ui

--- src/FilmLibrary.App/NewMovieForm.cs
+++ src/FilmLibrary.App/NewMovieForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FilmLibrary.Lib.Models;
+using FilmLibrary.Lib.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +14,33 @@ namespace FilmLibrary.App
 {
     public partial class NewMovieForm : Form
     {
+        private IImdbSearchService imdbSearchService = new ImdbSearchService();
+        private IMovieService movieService = new MovieService();
+
         public NewMovieForm()
         {
             InitializeComponent();
         }
 
-    
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var movie = imdbSearchService.SearchMovieImdbAsync(txtIMDBSearch.Text);
+        }
+
+        private void ShowMovieDetail(MovieModel movie)
+        {
+            txtTitle.Text = movie.Title;
+            txtReleaseDate.Text = movie.ReleaseDate.ToString("dd/MM/yyyy");
+            txtGenre.Text = movie.Genre;
+            txtDirector.Text = movie.Director;
+            txtActors.Text = movie.Actors;
+            rtxtDescription.Text = movie.Description;
+            txtIMDBRating.Text = movie.IMDBRating;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //movieService.AddNewMovieAsync(movie);
+        }
     }
 }

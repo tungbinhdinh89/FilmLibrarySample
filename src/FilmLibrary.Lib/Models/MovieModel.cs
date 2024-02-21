fff0ffd522035284cf2db0ae0fe695cc5e2d4bb5
Stephen Nguyen <tungbinhdinh89@gmail.com>
2024-02-21 13:12:18 +0700

    add movie service

---
+++ src/FilmLibrary.Lib/Models/MovieModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmLibrary.Lib.Models
+{
+    public class MovieModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Director { get; set; } = null!;
+        public int IMDBRating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
---
+++ src/FilmLibrary.Lib/Services/MovieService.cs
@@ -0,0 +1,17 @@
+﻿using FilmLibrary.Lib.Models;
+
+namespace FilmLibrary.Lib.Services
+{
+    public interface IMovieService
+    {
+        Task<MovieModel[]> SearchMoviesAsync(string keyword);
+    }
+
+    public class MovieService : IMovieService
+    {
+        public Task<MovieModel[]> SearchMoviesAsync(string keyword)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
